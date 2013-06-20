2fb38d244baae09932f5839113a55124784f8a81
vaibhav puri <vaibhav@taggito.com>
2013-06-20 21:30:02 +0200

    all additional files from the picture gem added

---
+++ app/controllers/static_controller.rb
@@ -0,0 +1,11 @@
+class StaticController < ApplicationController
+
+  def start
+    #do nothing in this function at the moment. The view renders links to the two functionalities of the app
+    
+
+
+  end
+
+
+end
---
+++ db/migrate/20130620154948_add_picture_to_place.rb
@@ -0,0 +1,5 @@
+class AddPictureToPlace < ActiveRecord::Migration
+  def change
+    add_column :places, :picture, :string
+  end
+end
